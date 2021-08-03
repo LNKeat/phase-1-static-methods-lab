e920aa990ba8227d21a438aae6a62cce37ebe1e9
Laura <lnkeat@gmail.com>
2021-08-03 17:29:30 -0600

    commit tree fd168efe5413aea1f8eca26a286942296a2b12ea

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
 class Formatter {
-  //add static methods here
+  constructor(str){
+    this.str = str
+  }
+  static capitalize(str){
+    return str.charAt(0).toUpperCase() + str.slice(1)
+  }
+  static sanitize(str){
+    return str.replace(/[^A-Za-z0-9- ']+/g, '');
+  }
+  static titleize(str){
+    let except = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    let strArr = str.split(' ')
+    let editedArr = []
+    for(let i = 0; i < strArr.length; i++){
+      let ele = strArr[i]
+      if(!except.includes(ele) || i === 0){
+        editedArr.push(this.capitalize(ele))
+      }else{
+        editedArr.push(ele)
+      }
+    }
+    return editedArr.join(' ')
+  }
 }
